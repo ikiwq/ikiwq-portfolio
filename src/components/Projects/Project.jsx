d489928b3dbc67ad6e95b292936770f4392d8559
Iki <ikiigaiiii@gmail.com>
2022-11-03 01:47:07 +0100

    Fixed some small issues

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -4,46 +4,58 @@ import "./Styles.scss"
 import { AiFillGithub, AiOutlineLink } from 'react-icons/ai'
 
 const Project = ({right, nightMode, project}) => {
+  const esRight = document.getElementsByClassName("start-right");
+  const esLeft = document.getElementsByClassName("start-left");
 
-      useEffect(()=>{
-        const esRight = document.getElementsByClassName("start-right");
-        const esLeft = document.getElementsByClassName("start-left");
+  useEffect(()=>{
 
-        for(let i=0;i<esRight.length;i++){
-          observerRight.observe(esRight[i]);
-        }
-  
-        for(let i=0;i<esLeft.length;i++){
-          observerLeft.observe(esLeft[i]);
-        }
-      },[])
+    for(let i=0;i<esRight.length;i++){
+      observerRight.observe(esRight[i]);
+    }
 
-      const observerLeft = new IntersectionObserver((entries)=>{
-        entries.forEach(entry=>{
-          if(entry.isIntersecting){
-            entry.target.classList.add("end-left");
-          }else{
-            entry.target.classList.remove("end-left");
-          }
-        })
-      })
+    for(let i=0;i<esLeft.length;i++){
+      observerLeft.observe(esLeft[i]);
+    }
+    
+    },[esRight, esLeft])
 
-      const observerRight = new IntersectionObserver((entries)=>{
-        entries.forEach(entry=>{
-          if(entry.isIntersecting){
-            entry.target.classList.add("end-right");
-          }else{
-            entry.target.classList.remove("end-right");
-          }
-        })
-      })
+  const observerLeft = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+      if(entry.isIntersecting){
+        entry.target.classList.add("end-left");
+      }else{
+        entry.target.classList.remove("end-left");
+      }
+    })
+  })
+
+  const observerRight = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+      if(entry.isIntersecting){
+        entry.target.classList.add("end-right");
+      }else{
+        entry.target.classList.remove("end-right");
+      }
+    })
+  })
+
+  const openLink = () => {
+    let url;
+    if( project.linkUrl){
+      url = project.linkUrl;
+    }else{
+      url = project.githubUrl;
+    }
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
+
   return (
     <div className="">
       <div className={`hidden ${right ? "justify-end" : "justify-start" } "items-end relative md:flex`}>
-        <div className='img-container relative w-4/6 hover:scale-110 transition-all duration-300'>
+        <div className='img-container relative w-4/6 hover:scale-110 hover:cursor-pointer transition-all duration-300' onClick={()=>{openLink()}}>
             <img src={project?.postImage?.url} className={`block w-full h-auto ${right ? "left-0 start-right": "right-0 start-left"} img-class`}/>
-            <div className={`absolute bottom-0 left-0 right-0 overflow-hidden h-full img-cover ${right ? "start-right": "start-left"}`}>
-            </div>
+            <div className={`absolute bottom-0 left-0 right-0 overflow-hidden h-full img-cover ${right ? "start-right": "start-left"}`}/>
         </div>
         <div className={`z-10 col-span-3 flex flex-col absolute ${ right ? "left-0 start-left" : "right-0 start-right"} md:top-0 xl:top-10 md:gap-4`}>
             <h1 className={`text-red-600 font-Bebas text-7xl ${right ? "text-start" : "text-end"} md:relative`}>{project.title}</h1>
@@ -59,7 +71,7 @@ const Project = ({right, nightMode, project}) => {
       </div>
       <div className={`flex flex-col ${right ? "items-end" : "items-start" } "justify-end relative md:hidden`}>
         <h1 className={`text-red-600 font-Bebas w-full text-7xl ${right ? "text-start start-right" : "text-end start-left"} md:relative`}>{project.title}</h1>
-        <img src={project?.postImage?.url} className={`w-4/5 ${right ? "left-0 start-right": "right-0 start-left"}`}/>
+        <img src={project?.postImage?.url} className={`w-4/5 ${right ? "left-0 start-right": "right-0 start-left"}`} onClick={()=>{openLink()}}/>
         <div className={`${ right ? "text-start left-0 start-left" : "text-end right-0 start-right"} px-2 text-white font-Source bg-red-600 max-w-xs absolute top-52 right-0 flex flex-col gap-2`}>
           <span className='text-sm'>{project?.projectDescription}</span>
           <span className='text-neutral-900 text-sm'>{project.technologies}</span>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Project from './Project'
 import {gql, useQuery} from '@apollo/client'
 import { PROJECT_QUERY } from '../../services'
 
  const Projects = ({nightMode}) => {
-
+ 
   const {data, loading, error} =  useQuery(PROJECT_QUERY);
   const projects = data?.projectsConnection.edges || [];
-
+  
   return (
     <div name="portfolio" className={`container max-w-6xl mx-auto px-4 py-4 ${ nightMode ? "bg-black" : "bg-white"} transition-all duration-500`}>
       <h1 className='text-red-600 font-Bebas text-7xl text-center md:text-end mt-10 md:mt-0'>MY PORTFOLIO</h1>
